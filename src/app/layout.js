7ba96d05f52778afd98d7657931a2f98e8926a98
1saifu <saifukhan@hotmail.se>
2024-09-09 20:09:43 +0200

    layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,6 +4,9 @@ import StoryblokProvider from "@/providers/StoryblokProvider";
 import { StoryblokCMS } from "@/utils/cms";
 import { storyblokInit, apiPlugin } from "@storyblok/react";
 
+// import Footer from "@/components/layout/Footer";
+// import Header from "@/components/layout/Header";
+
 storyblokInit({
   accessToken: StoryblokCMS.TOKEN,
   use: [apiPlugin],
@@ -11,11 +14,14 @@ storyblokInit({
 
 export default async function RootLayout({ children }) {
   const currentConfig = await StoryblokCMS.getConfig();
+
   return (
     <StoryblokProvider>
       <html>
         <body>
+          {/* <Header config={currentConfig} /> */}
           <Layout config={currentConfig}>{children}</Layout>
+          {/* <Footer config={currentConfig} /> */}
         </body>
       </html>
     </StoryblokProvider>
